9c744597c409cd254984a43889b619e92bc7e508
AlvarCrash <32309197+AlvarCrash@users.noreply.github.com>
2017-09-27 22:09:11 +0300

    Тестирование удаленной разработки

--- 407p/exit.php
+++ 407p/exit.php
@@ -4,6 +4,7 @@ unset($_SESSION['login']);
 unset($_SESSION['id']);
 session_destroy();
 header("Location:index.php");
+// Тестирование удаленной разработки =)
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
