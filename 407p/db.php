bafb1a67fa4a2b9a094cab5c3389d047a7e73d80
Andrey Udovenko <udovenkoaa@yandex.ru>
2017-09-27 19:09:56 +0300

    Тестовая фиксация

--- 407p/db.php
+++ 407p/db.php
@@ -3,9 +3,5 @@ $host = '127.0.0.1'; // адрес сервера
 $database = '407P'; // имя базы данных
 $user = 'root'; // имя пользователя
 $password = ''; // пароль
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
